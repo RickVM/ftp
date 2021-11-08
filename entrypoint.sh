85816b46c37a450c60e33f79f717a5a97121d497
RickVM <vanmelisrick@gmail.com>
2021-11-08 14:37:52 +0100

    UpdatE

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 echo "Helloa world"
 echo "$INPUT_HOST"
 
-run: lftp ftp://$INPUT_USERNAME@$INPUT_HOST -u $INPUT_USERNAME,$INPUT_PASSWORD -e "set ssl:verify-certificate false; mirror --reverse --verbose ./dist .;" bye
+lftp ftp://$INPUT_USERNAME@$INPUT_HOST -u $INPUT_USERNAME,$INPUT_PASSWORD -e "set ssl:verify-certificate false; mirror --reverse --verbose ./dist .;" bye
